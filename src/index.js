65cdb2ca3889fc782392a2a1d2241f81c2a3d145
Vitória Victor <vitoriavictor@Vitorias-MacBook-Pro.local>
2023-05-08 13:41:03 -0300

    funções do js funcionando, html base pronto, faltando aplicar css e implementar testes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//aqui você deve escutar os eventos de DOM, chamar cipher.encode() e cipher.decode()
+
+function cifrarMensagem() {
+    const textoInicial = document.getElementById('textoInicial').value.trim();
+    const deslocamento = parseInt(document.getElementById('deslocamento').value);
+    const textoFinal = cipher.encode(deslocamento, textoInicial);
+    document.getElementById('textoFinal').innerText = textoFinal;
+}
+const btnCifrar = document.getElementById('btn-cifrar');
+btnCifrar.addEventListener('click', cifrarMensagem);
+
+function decifrarMensagem() {
+    const textoFinal = document.getElementById('textoFinal').value.trim();
+    const deslocamento = parseInt(document.getElementById('deslocamento').value);
+    const textoInicial = cipher.decode(deslocamento, textoFinal);
+    document.getElementById('textoFinal').innerText = textoInicial;
+}
+const btnDecifrar = document.getElementById('btn-decifrar');
+btnDecifrar.addEventListener('click', decifrarMensagem);
+
+function limparMensagem() {
+    document.getElementById('textoInicial').value = '';
+    document.getElementById('deslocamento').value = 3;
+    document.getElementById('textoFinal').value = '';
+}
+
+const btnLimpar = document.getElementById('limpar');
+btnLimpar.addEventListener('click', limparMensagem);
+
